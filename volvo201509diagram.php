e0a7e83b80cd0f9fc9b40329cacc7eefb3787e2f
gyuri - sandbox-dev <szikoragy@vjsz.hu>
2015-09-30 16:24:48 +0200

    35napig zöld, átlagig sárga átlag felett piros.

--- volvo201509diagram.php
+++ volvo201509diagram.php
@@ -26,6 +26,8 @@ $sql .= "where psz ilike 'ncz%' and sorszam < 2000;";
 $res = $pg->query($sql);
 $row = $res->fetch(PDO::FETCH_BOTH);
 $avgTime = $row[0];
+// Tervezett Átfutási idő:
+$plannedTime = 35;
 
 $sql  = "select sorszam, substring(psz,1,6) as rendszam, erkezett, ";
 $sql .= "vegatvetel, vegatvetel-erkezett as nap from jarmu_alap ";
@@ -38,7 +40,7 @@ echo "<tr>
       <th>Sorszám</th>
       <th>Rendszám</th>
       <th>Dátumok</th>
-      <th>Átfutási idő (1 &diams; = 1 nap, átlag átfutási idő: $avgTime nap)</th>
+      <th>Átfutási idő (1 &diams; = 1 nap, átlag átfutási idő: $avgTime nap, tervezett átfutási idő 35 nap.)</th>
       </tr>";
 
 while ($row = $res->fetch(PDO::FETCH_BOTH)) {
@@ -48,11 +50,13 @@ while ($row = $res->fetch(PDO::FETCH_BOTH)) {
         <td>$row[2] &dash; $row[3] &dash; $row[4] nap</td>
         <td>";
           for ($i=0; $i<$row[4]; $i++) {
-
-            if ($i < $avgTime) {
+            if ($i <= $plannedTime) {
               echo "<b style='color:#007D00;font-size:75%;'>&diams;</b>";
             }
-            else {
+            if ($i > $plannedTime and $i <= $avgTime) {
+              echo "<b style='color:#f80;font-size:75%;'>&diams;</b>";
+            }
+            if ($i > $avgTime) {
               echo "<b style='color:#CA0000;font-size:75%;'>&diams;</b>";
             }
           }  // pontok ciklus vége
