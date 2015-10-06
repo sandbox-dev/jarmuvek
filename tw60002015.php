4561f919ba2db565cee2fecf61f09d847853804b
gyuri <szikoragy@gmail.com>
2015-10-06 19:52:23 +0200

    2015. évi Tw6000 kocsik.

---
+++ tw60002015.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+require_once 'autoloader.php';
+new Autoloader();
+$pg = Pg::getPg();
+
+
+// Kimenet tárazása
+ob_start();
+
+// Csatlakozás az adatbázishoz.
+
+
+// Html példány:
+$h = new Html();
+
+// Html fejléc
+$h->header("2015.  - TW6000");
+
+$h->msgOk("2015. évi TW6000  dátumok");
+
+$h->btnMenu(array("Átfutási idők - diagram"=>"tw60002015diagram.php",
+                   "Vissza a főmenühöz"=>"index.php"));
+
+$sql =  "select psz, ";
+$sql .= "sorszam, erkezett, allapotfelvetel, reszatvetel, vegatvetel, hazaadas ";
+$sql .= "from jarmu_alap where ev=2015 and jarmutipus ilike 'tw6000 j1' order by sorszam";
+$res = $pg->query($sql);
+// Van-e visszaadott sor
+$count = $res->rowCount();
+if ($res) {
+  if ($count) {
+    echo "<table class='jmu-info-table' border=1 style='border-collapse:collapse'>";
+    echo "<caption>2015. évi TW6000 kocsik</caption>";
+    echo "<tr>
+    <th>Pályaszám</th>
+    <th>Sorszám</th>
+    <th>Érkezett</th>
+    <th>Állapotfelvétel</th>
+    <th>Részátvétel</th>
+    <th>Végátvétel</th>
+    <th>Hazaadás</th>
+    </tr>";
+    while($row = $res->fetch(PDO::FETCH_BOTH)) {
+      echo "<tr>
+      <td class='tdc5p'>$row[0]</td>
+      <td class='tdc5p'>$row[1]</td>
+      <td class='tdc5p'>$row[2]</td>
+      <td class='tdc5p'>$row[3]</td>
+      <td class='tdc5p'>$row[4]</td>
+      <td class='tdc5p'>$row[5]</td>
+      <td class='tdc5p'>$row[6]</td>
+      </tr>";
+    }
+    echo "</table>";
+    
+  }
+  else {
+    $h->msgWarn("A kérés nem hozott eredményt!");
+  }
+}
+// Oldal alja gombok:
+$h->separator();
+$h->btnBack();
+$h->btnExit();
+$h->btnPrint();
+$h->footer();    
+  
+
+?>
---
+++ tw60002015diagram.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+require_once 'autoloader.php';
+new Autoloader();
+$pg = Pg::getPg();
+
+// Kimenet tárazása
+ob_start();
+
+
+
+// Html példány:
+$h = new Html();
+
+// Html fejléc
+$h->header("2015. évi TW6000 kocsik");
+
+$h->msgOk("2015. évi TW6000 kocsik átfutási idő diagramja");
+
+$h->btnMenu(array("Vissza a 2016. évi TW6000 kocsikhoz"=>"tw60002015.php",
+                   "Vissza a főmenühöz"=>"index.php"));
+
+// átlag átfutási idő meghatározása
+$sql  = "select (avg(vegatvetel-erkezett)::int) as atlag from jarmu_alap ";
+$sql .= "where ev=2015 and jarmutipus ilike 'tw6000 j1' ";
+
+$res = $pg->query($sql);
+$row = $res->fetch(PDO::FETCH_BOTH);
+$avgTime = $row[0];
+// Tervezett Átfutási idő:
+$plannedTime = 90;
+
+$sql  = "select sorszam, psz, erkezett, ";
+$sql .= "vegatvetel, vegatvetel-erkezett as nap from jarmu_alap ";
+$sql .= "where ev=2015 and jarmutipus ilike 'tw6000 j1' order by 1;";
+
+$res = $pg->query($sql);
+
+echo "<table border='1' style='border-collapse:collapse;'>";
+echo "<tr>
+      <th>Sorszám</th>
+      <th>Rendszám</th>
+      <th>Dátumok</th>
+      <th>Átfutási idő (1 &diams; = 1 nap, átlag átfutási idő: $avgTime nap, tervezett átfutási idő 90 nap.)</th>
+      </tr>";
+
+while ($row = $res->fetch(PDO::FETCH_BOTH)) {
+  echo "<tr>
+        <td>$row[0]</td>
+        <td>$row[1]</td> 
+        <td>$row[2] &dash; $row[3] &dash; $row[4] nap</td>
+        <td>";
+          for ($i=0; $i<$row[4]; $i++) {
+            if ($i < $plannedTime) {
+              echo "<b style='color:#007D00;font-size:75%;'>&diams;</b>";
+            }
+            if ($i >= $plannedTime and $i <= $avgTime) {
+              echo "<b style='color:#f80;font-size:75%;'>&diams;</b>";
+            }
+            if ($i > $avgTime) {
+              echo "<b style='color:#CA0000;font-size:75%;'>&diams;</b>";
+            }
+          }  // pontok ciklus vége
+
+  echo "</td>
+        </tr>";
+}
+echo "</table>";
+
+
+// Elválasztó
+$h->separator();
+// Oldal alja gombok:
+$h->btnBack();
+$h->btnExit();
+$h->btnPrint();
+
+// Html lábléc
+$h->footer();
+
+// Kimenet kiírása
+ob_flush();
+?>
+
